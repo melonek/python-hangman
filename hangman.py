7c54a093e404c799e84af455aab77a5906f37e08
melon <meloniusz@hotmail.com>
2022-04-25 15:38:35 +0800

    finalising the game

--- hangman.py
+++ hangman.py
@@ -35,42 +35,27 @@ def play(word):
                 if "_" not in word_completion:
                     guessed = True
         elif len(guess) == len(word) and guess.isalpha():
-            if guess in guessed_words
+            if guess in guessed_words:
+                print ("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
         else:
-        print("Guessed it wrong!")
-        print(display_hangman(tries))
-        print(word_completion)
-        print("\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            print("Guessed it wrong!")
+            print(display_hangman(tries))
+            print(word_completion)
+            print("\n")
+    if guessed:
+        print("Congrats, you guessed the word! You win!!!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
 
     def display_hangman(tries):
-    stages = [  # final state: head, torso, both arms, and both legs
+        stages = [  # final state: head, torso, both arms, and both legs
                 """
                    --------
                    |      |
@@ -142,3 +127,13 @@ def play(word):
                 """
     ]
     return stages[tries]
+
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? Y/N ").upper() == "Y":
+        word = get_word()
+        play(word)
+
+    if __name__ == "__main__":
+        main()
