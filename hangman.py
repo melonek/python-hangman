0413f20457d8988219bda7f73b58dc2c95468562
melon <meloniusz@hotmail.com>
2022-04-25 15:00:59 +0800

    randomizing the words

--- hangman.py
+++ hangman.py
@@ -1,1 +1,11 @@
-import
+import random
+from words import list_of_words
+
+def get_word():
+    word = random.choice(list_of_words)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed - False
+    guessed_letters = []
